560ad8faf20d8d1624a2f55fff1751aac7b13f38
Dave Trost <davetrost@gmail.com>
2019-11-01 13:37:32 -0700

    more setup

---
+++ src/services/api-service.js
@@ -0,0 +1,8 @@
+// export default const getApi = (url options) => {
+//   return fetch(``)
+//     .then(res => res.json())
+//     .then(({ results: characters }) => characters.map(character => ({
+//       name: character.name,
+//       image: character.image
+//     })));
+// };
