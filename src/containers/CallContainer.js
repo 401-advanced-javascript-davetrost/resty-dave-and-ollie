629816f0268b9729066620d538331ff5053dbc06
Dave Trost <davetrost@gmail.com>
2019-11-01 13:36:58 -0700

    container components

---
+++ src/containers/CallContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CallContainer() {
+  return (
+    <section>
+      <CallForm />
+      <Response />
+    </section>
+  );
+}
---
+++ src/containers/HistoryContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function HistoryContainer() {
+  return (
+    <section>
+      <h2>History</h2>
+      <div>
+      </div>
+    </section>
+  );
+}
