5b3b098e4bcaacd34398ec68e79ef657705746ee
Dave Trost <davetrost@gmail.com>
2019-11-03 09:42:12 -0800

    post functionality

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,10 +1,13 @@
+import stringToObject from './stringToObject';
+
 export const apiCall = (url, method, body) => {
-  let options = {};
+  const options = {
+    headers: { 'Content-Type': 'application/json' },
+  };
+  
   if(method === 'post') {
     options.method = 'POST';
-    options.body = body;
-    // options.mode = 'no-cors';
-    // console.log(options.body);
+    options.body = JSON.stringify(stringToObject(body));
   }
   
   return fetch(`${url}`, options)
