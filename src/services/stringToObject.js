60a9a217afc25390e753ef392eaece2563cb82f9
Dave Trost <davetrost@gmail.com>
2019-11-04 09:18:09 -0800

    allow single quotes and standard js in body text

--- src/services/stringToObject.js
+++ src/services/stringToObject.js
@@ -1,7 +1,7 @@
 export default function stringToObject(str) {
   /* regex to convert regular object notation to JSON string comes from SO
       https://stackoverflow.com/questions/1086404/string-to-object-in-js */
-  const jsonConvertedStr = str.replace('\'', '"').replace(/(\w+:)|(\w+ :)/g, matchedStr => {
+  const jsonConvertedStr = str.replace(/'/g, '"').replace(/(\w+:)|(\w+ :)/g, matchedStr => {
     return '"' + matchedStr.substring(0, matchedStr.length - 1) + '":';
   });
 
