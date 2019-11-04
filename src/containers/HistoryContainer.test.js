010ef343f58856c1e5a10811447c73f74a193ccd
Dave Trost <davetrost@gmail.com>
2019-11-04 10:02:02 -0800

    container tests

--- src/containers/HistoryContainer.test.js
+++ src/containers/HistoryContainer.test.js
@@ -3,16 +3,16 @@ import { shallow } from 'enzyme';
 import HistoryContainer from './HistoryContainer';
 
 describe('History Container component', () => {
-  const sampleHistoryItem = {
-    _id: 'xyzabc',
-    url: 'random-api.com/api/characters',
-    method: 'post',
-    body: `{
-      name: 'Garfield',
-      color: 'Orange with black stripes'
-    }`
-  };
   it('renders the container', () => {
+    const sampleHistoryItem = {
+      _id: 'xyzabc',
+      url: 'random-api.com/api/characters',
+      method: 'post',
+      body: `{
+        name: 'Garfield',
+        color: 'Orange with black stripes'
+      }`
+    };
     const wrapper = shallow(<HistoryContainer 
       items={[sampleHistoryItem, sampleHistoryItem]}
       handleHistorySelect={() => true}
